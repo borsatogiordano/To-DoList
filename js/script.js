633729bd4583567d610614cfa9d2c120cfb079fa
Giordano Borsato <borsatogiordano@gmail.com>
2024-08-30 21:39:19 -0300

    Js funcionando

--- js/script.js
+++ js/script.js
@@ -1,63 +1,113 @@
 
-
-const input = document.querySelector("#addTask");
-
-const button = document.querySelector("#add-button")
-
+const input = document.querySelector('#task')
+const button = document.querySelector("#button_create")
 const ul = document.querySelector(".task-list")
 
-const dbKey = "@task-list"
-
-const array = [{ description: "sexo2", taskCompleted: false }]
+const DB_KEY = "@test";
 
 const STORAGE_SERVICE = {
     listTasks: () => {
-        localStorage.setItem(dbKey, JSON.stringify(array))
-        const storage = localStorage.getItem(dbKey)
-
-        if (storage) {
-            return console.log(JSON.parse(storage))
-        }
-
-        return []
+        const storage = localStorage.getItem(DB_KEY);
+        return storage ? JSON.parse(storage) : [];
     },
-
-    // O que é task? .. Será o nosso objeto desde formato {description: string. taskCompleted: boolean}
     createTask: (taskDescription) => {
-
-        const storage = localStorage.getItem(dbKey)
-
-        const newTask = {
-            description: taskDescription,
-            taskCompleted: false,
-        }
-
+        const storage = localStorage.getItem(DB_KEY);
+        const newTask = { description: taskDescription, isCompleted: false };
         if (storage) {
-            const storageParsed = JSON.parse(storage)
-
-            const tasks = {
-                ...storageParsed, newTask
-            }
-
-            return localStorage.setItem(dbKey, JSON.stringify({ tasks }))
+            const storageParsed = JSON.parse(storage);
+            const tasks = [...storageParsed, newTask];
+            localStorage.setItem(DB_KEY, JSON.stringify(tasks));
+        } else {
+            localStorage.setItem(DB_KEY, JSON.stringify([newTask]));
         }
-        return localStorage.setItem(dbKey, JSON.stringify({ tasks: [newTask] }))
-    }
-}
+    },
+    deleteTask: (taskDescription) => {
+        const storage = localStorage.getItem(DB_KEY);
+        if (storage) {
+            const storageParsed = JSON.parse(storage);
+            const filteredTasks = storageParsed.filter(
+                (item) => item.description !== taskDescription
+            );
+            localStorage.setItem(DB_KEY, JSON.stringify(filteredTasks));
+        } else {
+            alert("Task not found");
+        }
+    },
+    updateTaskState: (taskDescription) => {
+        const storage = localStorage.getItem(DB_KEY);
+        if (storage) {
+            const storageParsed = JSON.parse(storage);
+            const updatedTask = storageParsed.map((item) => {
+                if (item.description === taskDescription) {
+                    return { ...item, isCompleted: !item.isCompleted };
+                }
+                return item;
+            });
+            localStorage.setItem(DB_KEY, JSON.stringify(updatedTask));
+        } else {
+            alert("Task not found");
+        }
+    },
+};
 
+document.addEventListener("DOMContentLoaded", () => updateActivityList());
 
+const addTaskInput = document.getElementById("addTask");
+const addTaskButton = document.getElementById("add-button");
+const taskList = document.getElementById("task-list");
 
-document.addEventListener("DOMContentLoaded", () => { });
-
-button.addEventListener("click", (event) => {
-
+addTaskButton.addEventListener("click", (event) => {
     event.preventDefault();
-    if (!input.value) {
-        return alert("A tarefa precisa ser preenchida")
+    if (!addTaskInput.value) {
+        return alert("A tarefa precisa ser preenchida");
     }
-    const checkbox = `<input type="checkbox"/>`
-    const li = `<li class="task-item"> ${checkbox} <p> ${input.value} </p> </li>`
-
-    ul.innerHTML += li
+    STORAGE_SERVICE.createTask(addTaskInput.value);
+    updateActivityList();
+    addTaskInput.value = "";
 });
 
+const updateStateTask = (event) => {
+    const input = event.target;
+    const description = input.value;
+    STORAGE_SERVICE.updateTaskState(description);
+};
+
+const createTaskItem = (task) => {
+    let checkbox = `<input type="checkbox" value="${task.description}" `;
+    if (task.isCompleted) {
+        checkbox += "checked";
+    }
+    checkbox += `>`;
+    const li = `<li class="task-item">
+      ${checkbox}
+      <p>${task.description}</p>
+    </li>`;
+    return li;
+};
+
+// Adicione o evento de clique ao checkbox
+taskList.addEventListener("click", (event) => {
+    if (event.target.type === "checkbox") {
+        const description = event.target.value;
+        STORAGE_SERVICE.updateTaskState(description);
+        updateActivityList();
+    }
+});
+
+const updateActivityList = () => {
+    const taskList = document.getElementById("task-list");
+    const tasks = STORAGE_SERVICE.listTasks();
+    const countTasks = tasks.length;
+    const countTasksCompleted = tasks.filter((item) => item.isCompleted).length;
+    document.querySelector(".count-tasks").textContent = countTasks;
+    document.querySelector(".count-finished").textContent = countTasksCompleted;
+    taskList.innerHTML = "";
+    if (tasks.length === 0) {
+        const emptyState = document.querySelector(".empty-space");
+        emptyState.innerHTML = "<p>Nenhuma tarefa cadastrada.</p>";
+    } else {
+        for (let task of tasks) {
+            taskList.innerHTML += createTaskItem(task);
+        }
+    }
+};
